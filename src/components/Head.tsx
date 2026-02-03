7c06480eea25a7d6517c3259d1c23391e62a23c3
kekachi Ekeke <161198822+Ghost5072@users.noreply.github.com>
2026-02-03 15:41:02 +0100

    favicon

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -4,7 +4,7 @@ export const Head = () => {
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       {/* Favicon */}
-      <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      <link rel="icon" type="image/x-icon" href="/public/favicon.ico" />
       {/* Fonts */}
       <link rel="preconnect" href="https://static.parastorage.com" />
     </>
